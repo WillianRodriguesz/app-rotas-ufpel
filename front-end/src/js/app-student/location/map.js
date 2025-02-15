4c316c1d16a396d341568f1b74da84597bbcedd9
Willian <willianrodrigues@mertins.com.br>
2025-02-15 14:37:22 -0300

    refac(map): refatorando codigo

--- front-end/src/js/app-student/location/map.js
+++ front-end/src/js/app-student/location/map.js
@@ -1,15 +1,7 @@
 import mockLocations from "../../mocks/location.js";
 import paradas from "../../mocks/paradas.js";
 
-const map = L.map('map').setView([-31.780832297261984, -52.323695006471866], 20); 
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CartoDB</a>',
-}).addTo(map);
-
-let markers = [];
-let routes = [];
-
+// Definição dos ícones antes de usá-los
 const busIcon = L.icon({
     iconUrl: '/public/img/iconBus.png', 
     iconSize: [58, 60],         
@@ -17,13 +9,15 @@ const busIcon = L.icon({
     popupAnchor: [0, -32]       
 });
 
-// Define um ícone básico para as paradas
 const paradaIcon = L.icon({
     iconUrl: '/public/img/iconParada.png', 
     iconSize: [25, 47],         
     iconAnchor: [20, 20],       
     popupAnchor: [-5, -15]       
 });
+
+// Inicialização do mapa
+const map = L.map('map').setView([-31.780832297261984, -52.323695006471866], 20); 
 
 // Função para adicionar um marcador
 function addMarker(lat, lon, message, useCustomIcon = false, icon = undefined) {
@@ -37,29 +31,20 @@ function addMarker(lat, lon, message, useCustomIcon = false, icon = undefined) {
     return marker; 
 }
 
-// Função para adicionar uma rota
-function addRoute(latlngs, color = 'blue') {
-    const polyline = L.polyline(latlngs, { color }).addTo(map);
-    routes.push(polyline); 
-    map.fitBounds(polyline.getBounds());
-}
-
-// Função para limpar o mapa
-function clearMap() {
-    markers.forEach(marker => map.removeLayer(marker));
-    markers = [];
-
-    routes.forEach(route => map.removeLayer(route));
-    routes = [];
-}
-
-// Adiciona o marcador inicial com ícone customizado (ônibus)
+let markers = [];
 const marker = addMarker(-31.780157095467167, -52.323734327957766, "Rota Anglo", true);
 
-// Adiciona marcadores para as paradas
-paradas.forEach(({ position, parada }) => {
-    addMarker(position[0], position[1], parada, false, paradaIcon);
-});
+// Adicionando o layer do mapa
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CartoDB</a>',
+}).addTo(map);
+
+// Função para adicionar as paradas no mapa
+function addParadasMapa(paradas){
+    paradas.forEach(({ position, parada }) => {
+        addMarker(position[0], position[1], parada, false, paradaIcon);
+    });
+}
 
 // Função para simular atualizações de localização
 let currentLocationIndex = 0;
@@ -73,7 +58,8 @@ function simulateLocationUpdates() {
         } else {
             currentLocationIndex = 0; 
         }
-    }, 2000); // Atualiza a cada 2 segundos
+    }, 2000); 
 }
 
 simulateLocationUpdates();
+addParadasMapa(paradas);
